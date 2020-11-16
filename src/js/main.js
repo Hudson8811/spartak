34306c2e2e0fcee6e20e80972d6edab72f874c6c
ArtZ <snickgem@gmail.com>
2020-11-16 21:11:26 +0400

    index

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/jquery.fancybox.min.js
+//= libs/slick.min.js
+//= libs/jquery.marquee.min.js
+//= libs/jquery.mask.min.js
+//= libs/select2.full.min.js
+//= libs/jquery.nicescroll.min.js
+//= libs/wow.min.js
+
+/* my scripts */
+
+//= partials/header.js
